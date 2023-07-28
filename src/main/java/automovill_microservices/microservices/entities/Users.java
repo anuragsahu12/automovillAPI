8a99e28a447145d5de7adf68541bce797d2e718f
Anurag Sahu <anishsahu33@gmail.com>
2023-07-28 19:45:13 +0530

    add the folder structures

---
+++ src/main/java/automovill_microservices/microservices/entities/Users.java
@@ -0,0 +1,55 @@
+package automovill_microservices.microservices.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
+
+    private String username;
+    private String password;
+    private boolean is_admin;
+   
+   
+   
+   
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public boolean isIs_admin() {
+        return is_admin;
+    }
+    public void setIs_admin(boolean is_admin) {
+        this.is_admin = is_admin;
+    }
+    public Users(String id, String username, String password, boolean is_admin) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.is_admin = is_admin;
+    }
+    
+
+}
