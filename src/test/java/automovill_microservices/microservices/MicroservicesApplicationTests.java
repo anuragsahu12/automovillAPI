b0b5a39692dc41b9c2bd8500a8d4a00e252dea4e
Anurag Sahu <anishsahu33@gmail.com>
2023-07-28 19:02:33 +0530

    made the maven project

---
+++ src/test/java/automovill_microservices/microservices/MicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package automovill_microservices.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
