461bbe5d4c8edc60e16a053538f81b11c3adfe9b
Zubayer Ahmed Zidhan <zubaeerahmed@gmail.com>
2023-07-28 23:56:41 +0530

    Fixed an Error (missed Servie annotation)

--- src/main/java/automovill_microservices/microservices/controller/UserController.java
+++ src/main/java/automovill_microservices/microservices/controller/UserController.java
@@ -5,8 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import automovill_microservices.microservices.entities.Users;
 
+import automovill_microservices.microservices.entities.Users;
 import automovill_microservices.microservices.services.UserService;
 
 @RestController
--- src/main/java/automovill_microservices/microservices/services/impl/UserServiceImpl.java
+++ src/main/java/automovill_microservices/microservices/services/impl/UserServiceImpl.java
@@ -3,11 +3,13 @@ package automovill_microservices.microservices.services.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import automovill_microservices.microservices.entities.Users;
 import automovill_microservices.microservices.repository.UserRepository;
 import automovill_microservices.microservices.services.UserService;
 
+@Service
 public class UserServiceImpl implements UserService {
     @Autowired
     UserRepository userRepository;
