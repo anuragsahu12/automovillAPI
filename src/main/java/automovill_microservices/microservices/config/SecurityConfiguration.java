6523856d1798c05ca83332d467a54a314aed1384
Zubayer Ahmed Zidhan <zubaeerahmed@gmail.com>
2023-08-14 17:16:50 +0530

    remove not needed dependency

--- src/main/java/automovill_microservices/microservices/config/SecurityConfiguration.java
+++ src/main/java/automovill_microservices/microservices/config/SecurityConfiguration.java
@@ -1,6 +1,5 @@
 package automovill_microservices.microservices.config;
 
-import java.util.Arrays;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
