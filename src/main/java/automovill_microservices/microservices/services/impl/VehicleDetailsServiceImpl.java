8face857b3ac6745a6d3e2162651e9888aa43f7a
Zubayer Ahmed Zidhan <zubaeerahmed@gmail.com>
2023-08-22 21:55:44 +0530

    All Basic Details

--- src/main/java/automovill_microservices/microservices/services/impl/VehicleDetailsServiceImpl.java
+++ src/main/java/automovill_microservices/microservices/services/impl/VehicleDetailsServiceImpl.java
@@ -14,17 +14,28 @@ import automovill_microservices.microservices.services.VehicleDetailsService;
 public class VehicleDetailsServiceImpl implements VehicleDetailsService {
     @Autowired
     private VehicleDetailsRepository vehicleDetailsRepository;
-    
-    public List<VehicleDetails> getVehicleDetails(){
+
+    public List<VehicleDetails> getVehicleDetails() {
         return vehicleDetailsRepository.findAll();
     }
 
     @Override
     public VehicleDetails getVehicleDetailsByChassisNum(String chassisNum) {
         Optional<VehicleDetails> optionalVehicle = vehicleDetailsRepository.findById(chassisNum);
-        return optionalVehicle.orElse(null);
-    }    
 
+        if (optionalVehicle != null) {
+            // Dusra kch jisse make, model, amc type, mile jaaye
+            // 1 query se make, model using optionalVehicle.basic_details_id
+            
+            // 1 query for amc_type, using optionalVehicle.amc_id
 
-    
+            // AllBasicDetails allBasicDetails;
+            return optionalVehicle.orElse(null);
+
+        }
+        else {
+            return null;
+        }
+    }
+
 }
