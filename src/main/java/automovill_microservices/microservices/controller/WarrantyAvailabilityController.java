99f264c252bbace1e2dc0b41f61237b5a78722b5
Zubayer Ahmed Zidhan <zubaeerahmed@gmail.com>
2023-09-10 00:44:37 +0530

    add CrossOrigin

--- src/main/java/automovill_microservices/microservices/controller/WarrantyAvailabilityController.java
+++ src/main/java/automovill_microservices/microservices/controller/WarrantyAvailabilityController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import automovill_microservices.microservices.others.WarrantyAvailabilityDetails
 import automovill_microservices.microservices.others.WarrantyScopesResponse;
 import automovill_microservices.microservices.services.WarrantyAvailabilityService;
 
+@CrossOrigin(value = "localhost:19000")
 @RestController
 @RequestMapping("api/v1/warranty-availability")
 public class WarrantyAvailabilityController {
