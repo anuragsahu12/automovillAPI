73b1726da6a184b0b29be977bd2bd90b3f87571c
Zubayer Ahmed Zidhan <zubaeerahmed@gmail.com>
2023-09-11 22:02:29 +0530

    fix bug

--- src/main/java/automovill_microservices/microservices/services/impl/AutomovillHomesServiceImpl.java
+++ src/main/java/automovill_microservices/microservices/services/impl/AutomovillHomesServiceImpl.java
@@ -3,11 +3,13 @@ package automovill_microservices.microservices.services.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import automovill_microservices.microservices.entities.AutomovillHomes;
 import automovill_microservices.microservices.repository.AutomovillHomesRepository;
 import automovill_microservices.microservices.services.AutomovillHomesService;
 
+@Service
 public class AutomovillHomesServiceImpl implements AutomovillHomesService {
 
     @Autowired
