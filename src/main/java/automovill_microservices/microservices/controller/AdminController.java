8f4adb0e313a0fd4c7b8af8348748b5e3b8b99b7
Zubayer Ahmed Zidhan <zubaeerahmed@gmail.com>
2023-09-16 03:21:55 +0530

    update to show success message on successfull add

--- src/main/java/automovill_microservices/microservices/controller/AdminController.java
+++ src/main/java/automovill_microservices/microservices/controller/AdminController.java
@@ -2,6 +2,8 @@ package automovill_microservices.microservices.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,9 +24,14 @@ public class AdminController {
     AdminService adminService;
 
     @PostMapping("/addNewVehicle")
-    public void addNewVehicle(
+    public ResponseEntity<String> addNewVehicle(
         @RequestBody AddNewVehicleRequest request
     ) {
-        adminService.initializeNewVehicleData(request);
+        try {
+            adminService.initializeNewVehicleData(request);
+            return new ResponseEntity<String>("Added Successfully", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<String>("Failed", HttpStatus.BAD_REQUEST);
+        }
     }
 }
