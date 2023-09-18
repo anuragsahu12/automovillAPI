515bf0097d6dfad3117970a207e2d209a4724a5c
Zubayer Ahmed Zidhan <zubaeerahmed@gmail.com>
2023-09-18 20:25:38 +0530

    sort by date

--- src/main/java/automovill_microservices/microservices/others/InvoiceDetailsChassis.java
+++ src/main/java/automovill_microservices/microservices/others/InvoiceDetailsChassis.java
@@ -10,12 +10,18 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class InvoiceDetailsChassis {
+public class InvoiceDetailsChassis implements Comparable<InvoiceDetailsChassis>{
     private String bill_number;
     private String date_of_booking;
     private Object services;
     private float total_cost;
     private String workshop_id;
     private int distance_travelled;
+
+    @Override
+    public int compareTo(InvoiceDetailsChassis other) {
+        return this.date_of_booking.compareTo(other.date_of_booking);
+    }
+
 }
 
